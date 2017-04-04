4c8164152bce08109bde95724e7f7ae8b504176d
John Muse <john.r.muse@gmail.com>
2017-04-04 13:50:31 -0400

    Started on new project of organization and moving robot to subsystem.

---
+++ src/com/team5854/frc2017/Constants.java
@@ -0,0 +1,17 @@
+package com.team5854.frc2017;
+
+/**
+ * Class contains constants used by the rest of the robot code.
+ */
+public class Constants {
+	
+	// talon srx can ID ports
+	public static final int LEFT_FRONT_MASTER_ID = 0;
+	public static final int RIGHT_FRONT_MASTER_ID = 0;
+	public static final int LEFT_REAR_MASTER_ID = 0;
+	public static final int RIGHT_REAR_MASTER_ID = 0;
+	public static final int LEFT_FRONT_SLAVE_ID = 0;
+	public static final int RIGHT_FRONT_SLAVE_ID = 0;
+	public static final int LEFT_REAR_SLAVE_ID = 0;
+	public static final int RIGHT_REAR_SLAVE_ID = 0;
+}
---
+++ src/com/team5854/frc2017/subsystems/Drive.java
@@ -0,0 +1,62 @@
+package com.team5854.frc2017.subsystems;
+
+import java.lang.invoke.ConstantCallSite;
+
+import com.ctre.CANTalon;
+import com.team5854.frc2017.Constants;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Drive extends Subsystem {
+
+	// objects representing components of the drivertrain
+	private final CANTalon leftFrontMaster, rightFrontMaster, leftRearMaster, rightRearMaster; // declare the master motor controllers
+	private final CANTalon leftFrontSlave, rightFrontSlave, leftRearSlave, rightRearSlave;  // delcare slave motor controllers
+	
+	
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * Initializes all components of the drivetrain.
+	 */
+	private Drive() {
+		//set up the ports
+		leftFrontMaster = new CANTalon(Constants.LEFT_FRONT_MASTER_ID);
+		rightFrontMaster = new CANTalon(Constants.RIGHT_FRONT_MASTER_ID);
+		leftRearMaster = new CANTalon(Constants.LEFT_REAR_MASTER_ID);
+		rightRearMaster = new CANTalon(Constants.RIGHT_REAR_MASTER_ID);
+		leftFrontSlave = new CANTalon(Constants.LEFT_FRONT_SLAVE_ID);
+		rightFrontSlave = new CANTalon(Constants.RIGHT_FRONT_SLAVE_ID);
+		leftRearSlave = new CANTalon(Constants.LEFT_REAR_SLAVE_ID);
+		rightRearSlave = new CANTalon(Constants.RIGHT_REAR_SLAVE_ID);
+		
+		
+		// set the initial control modes for each Talon
+		
+		//sets the initial control mode for the master talons to PercentVBus and stopped.
+		leftFrontMaster.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
+		rightFrontMaster.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
+		leftRearMaster.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
+		rightRearMaster.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
+		leftFrontMaster.set(0.0);
+		rightFrontMaster.set(0.0);
+		leftRearMaster.set(0.0);
+		rightRearMaster.set(0.0);
+		
+		// sets the initial control mode for the slave talons to Follower and the ids they should follow
+		leftFrontSlave.changeControlMode(CANTalon.TalonControlMode.Follower);
+		leftFrontSlave.set(Constants.LEFT_FRONT_MASTER_ID);
+		rightFrontSlave.changeControlMode(CANTalon.TalonControlMode.Follower);
+		rightFrontSlave.set(Constants.RIGHT_FRONT_MASTER_ID);
+		leftRearSlave.changeControlMode(CANTalon.TalonControlMode.Follower);
+		leftRearSlave.set(Constants.LEFT_REAR_MASTER_ID);
+		rightRearSlave.changeControlMode(CANTalon.TalonControlMode.Follower);
+		rightRearSlave.set(Constants.RIGHT_REAR_MASTER_ID);
+		
+		
+	}
+}
